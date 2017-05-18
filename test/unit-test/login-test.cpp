273db3e3b04352ec5308ec9710214554c8b5e5b5
jezznar <jessnarsinues@gmail.com>
2017-05-18 19:25:58 +0800

    Initial Unit Testing Framework

---
+++ test/unit-test/login-test.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../../src/login/login.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unittest
+{		
+	TEST_CLASS(loginchrif)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+	TEST_CLASS(login)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
